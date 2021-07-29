832236e56d60a5cdeb8914f7dc57d70f8def6260
Yashwanth K <yashwanthkkn@gmail.com>
2021-07-29 13:14:35 +0530

    added validation to register form ui

--- src/components/Auth/Register.jsx
+++ src/components/Auth/Register.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
-import { Row, Col ,Typography, Space, Button, Input, Select } from 'antd';
+import { Row, Col ,Typography, Space, Button,Form, Input, Select } from 'antd';
 import { GoogleOutlined ,FacebookOutlined} from '@ant-design/icons';
 const { Title, Text } = Typography;
 const { Option } = Select;
 
 function Register() {
+    const [form] = Form.useForm();
+
+    const onSubmit = (values)=>{
+      console.log(values);
+    }
+
     return (
         <>
         <Typography className="mt-5">
@@ -13,7 +19,122 @@ function Register() {
             <Title level={3}> Create Account</Title>
           </Space>
         </Typography>
-        <Row justify="space-between">
+        <Form
+           form={form}
+          name='login'
+          onFinish={onSubmit}
+        >
+          <Row justify="space-between">
+            <Col xs={24} lg={11}>
+              <Form.Item
+                name="fname"
+                rules={[
+                  { required: true, message: 'Please enter your First Name ' },
+                ]}
+              >
+                <Input allowClear placeholder="First Name"  />
+              </Form.Item>
+            </Col>
+            <Col xs={24} lg={11}>
+              <Form.Item
+                name="lname"
+                rules={[
+                  { required: true, message: 'Please enter your Last Name ' },
+                ]}
+              >
+                <Input allowClear placeholder="Last Name"  />
+              </Form.Item>
+            </Col>
+          </Row>
+          <Row justify="space-between">
+            <Col xs={11}>
+              <Form.Item
+                name="gender"
+                rules={[{ required: true , message:"Choose your gender" }]}
+              >
+                <Select placeholder='Choose'>
+                  <Option value="Male">Male</Option>
+                  <Option value="Female">Female</Option>
+                  <Option value="Other">Other</Option>
+                </Select>
+              </Form.Item>
+            </Col>
+            <Col xs={11}>
+              <Form.Item
+                name="businessType"
+                rules={[{ required: true , message:"Choose your business type"  }]}
+              >
+                <Select placeholder='Choose' className='w-100'>
+                    <Option value="Retailer">Retailer</Option>
+                    <Option value="Freelancer">Freelancer</Option>
+                </Select>
+              </Form.Item>
+            </Col>
+          </Row>
+          <Form.Item
+            name="business"
+            rules={[
+              { required: true, message: 'Please enter your Business Name ' },
+            ]}
+          >
+            <Input allowClear placeholder="Business Name"  />
+          </Form.Item>
+
+          <Form.Item
+            name="email"
+            rules={[
+              { required: true, message: 'Please enter your Email id ' },
+              { type:'email' , message:"Enter a valid Email id" }
+            ]}
+          >
+            <Input allowClear placeholder="Email"  />
+          </Form.Item>
+
+          <Row justify="space-between">
+            <Col xs={24} lg={11}>
+              <Form.Item
+                name="password"
+                rules={[
+                  { required: true, message: 'Please enter your Password '},
+                  { min:8 , message:'Password must have atleast 8 characters'}
+                ]}
+              >
+                <Input.Password placeholder="Password"  />
+              </Form.Item>      
+            </Col>
+            <Col xs={24} lg={11}>
+              <Form.Item
+                  name="confirm"
+                  dependencies={['password']}
+                  hasFeedback
+                  rules={[
+                    {
+                      required: true,
+                      message: 'Please confirm your Password!',
+                    },
+                    ({ getFieldValue }) => ({
+                      validator(_, value) {
+                        if (!value || getFieldValue('password') === value) {
+                          return Promise.resolve();
+                        }
+                        return Promise.reject(new Error('The two passwords that you entered do not match!'));
+                      },
+                    }),
+                  ]}
+                >
+                <Input.Password placeholder='Confirm Password' />
+              </Form.Item>
+            </Col>
+          </Row>
+
+          <Form.Item className="text-center">
+              <Button className="rounded-btn " shape="round" type="primary" htmlType='submit'>
+                  Register
+              </Button>
+          </Form.Item>
+        </Form>
+
+        {/* <Row justify="space-between">
           <Col xs={24} md={11}>
             <Input placeholder="First Name" allowClear className="mt-3" />    
           </Col>
@@ -46,7 +167,7 @@ function Register() {
                 Register
             </Button>
           </Link>
-        </div>
+        </div> */}
         <Typography className="text-center mt-5">
           <Text type="secondary"> OR </Text>
         </Typography>
