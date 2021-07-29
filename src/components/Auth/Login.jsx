ce56a1ec440761ce5d26a556a4d9b872754e91d6
Yashwanth K <yashwanthkkn@gmail.com>
2021-07-29 11:46:10 +0530

    added handlers to login form

--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -1,10 +1,40 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {Link} from 'react-router-dom'
 import { Row, Col ,Typography, Space, Button, Input } from 'antd';
 import { GoogleOutlined ,FacebookOutlined} from '@ant-design/icons';
 const { Title, Text } = Typography;
 
 function Login() {
+    const [email , setEmail] = useState({
+      value:'',
+      err:false,
+      msg:'',
+      clicked:false
+    });
+    const [password,setPassword] = useState({
+      value:'',
+      err:false,
+      msg:'',
+      clicked:false
+    });
+
+    const handleEmail = (e)=>{
+      setEmail({value:e.target.value})
+    }
+
+    const handlePassword = (e)=>{
+      setPassword({value:e.target.value})
+    }
+
+
+    const handleSubmit = ()=>{
+      
+      let data = {
+        email:email.value,
+        password:password.value
+      }
+      console.log(data);
+    }
     return (
       <>
         <Typography className="mt-5">
@@ -15,15 +45,29 @@ function Login() {
             </Text>
           </Space>
         </Typography>
-        <Input placeholder="Email" allowClear className="mt-5 w-100" />
-        <Input.Password placeholder="Password" className="mt-3 w-100" />
-        <div className="text-center">
-            <Link to="/home">
-                <Button className="mt-4 rounded-btn " shape="round" type="primary">
-                    Login
-                </Button>
-            </Link>
-        </div>
+        {/* LOGIN FORM */}
+        <form>
+          <Input 
+            value={email.value} 
+            onChange={ handleEmail } 
+            placeholder="Email" 
+            allowClear 
+            className="mt-5 w-100"
+          />
+          { email.err && <Text type='danger'>{email.msg}</Text> }
+          <Input.Password 
+            value={password.value} 
+            onChange={ handlePassword } 
+            placeholder="Password" 
+            className="mt-3 w-100" 
+          />
+          { password.err && <Text type='danger'>{password.msg}</Text> }
+          <div className="text-center">
+              <Button onClick={handleSubmit} className="mt-4 rounded-btn " shape="round" type="primary">
+                  Login
+              </Button>
+          </div>
+        </form>
         <Typography className="text-center mt-5">
           <Text type="secondary"> OR </Text>
         </Typography>
