7f324c1cef7b043e5ba3b0fd9fdfc635f1551a26
Yashwanth K <yashwanthkkn@gmail.com>
2021-08-02 22:11:31 +0530

    updated register UI

--- components/Auth/Register.jsx
+++ components/Auth/Register.jsx
@@ -1,7 +1,6 @@
-import {Row, Col, Typography, Space, Button, Form, Input, Select, message} from 'antd';
+import { Row, Col ,Typography, Space, Button,Form, Input, Select } from 'antd';
 import { GoogleOutlined ,FacebookOutlined} from '@ant-design/icons';
 import axios from "axios";
-import {FacebookLoginButton, GoogleLoginButton} from "react-social-login-buttons";
 const { Title, Text } = Typography;
 const { Option } = Select;
 
@@ -10,29 +9,7 @@ function Register() {
 
     const onSubmit = (payload)=>{
       console.log(payload);
-        const hide=message.loading('Please wait...',0);
-        axios.post('/auth/register',payload)
-            .then(res=>{
-                if(res.status===200)
-                {
-                    console.log(res)
-                    console.log(res.data.token);
-                    hide();
-                    message.success('Register Successful',2);
-                }
-                else{
-                    throw Error(res.data.msg);
-                }
-            })
-            .catch(err=>{
-                hide();
-                if (!err.response) {
-                    console.log("Custom Network Error")
-                    message.error('Network Error',2);
-                } else {
-                    message.error(err.message,2)
-                }
-            });
+      axios.post('/auth/register',payload).then(res=>console.log(res));
     }
 
     return (
@@ -42,22 +19,39 @@ function Register() {
             <Title level={3}> Create Account</Title>
           </Space>
         </Typography>
-        <Form form={form} name='login' onFinish={onSubmit}>
+        <Form
+           form={form}
+          name='login'
+          onFinish={onSubmit}
+        >
           <Row justify="space-between">
             <Col xs={24} lg={11}>
-              <Form.Item name="fname" rules={[{ required: true, message: 'Please enter your First Name ' },]}>
+              <Form.Item
+                name="fname"
+                rules={[
+                  { required: true, message: 'Please enter your First Name ' },
+                ]}
+              >
                 <Input allowClear placeholder="First Name"  />
               </Form.Item>
             </Col>
             <Col xs={24} lg={11}>
-              <Form.Item name="lname" rules={[{ required: true, message: 'Please enter your Last Name ' },]}>
+              <Form.Item
+                name="lname"
+                rules={[
+                  { required: true, message: 'Please enter your Last Name ' },
+                ]}
+              >
                 <Input allowClear placeholder="Last Name"  />
               </Form.Item>
             </Col>
           </Row>
           <Row justify="space-between">
             <Col xs={11}>
-              <Form.Item name="gender" rules={[{ required: true , message:"Choose your gender" }]}>
+              <Form.Item
+                name="gender"
+                rules={[{ required: true , message:"Choose your gender" }]}
+              >
                 <Select placeholder='Choose'>
                   <Option value="M">Male</Option>
                   <Option value="F">Female</Option>
@@ -66,7 +60,10 @@ function Register() {
               </Form.Item>
             </Col>
             <Col xs={11}>
-              <Form.Item name="btype" rules={[{ required: true , message:"Choose your business type"  }]}>
+              <Form.Item
+                name="btype"
+                rules={[{ required: true , message:"Choose your business type"  }]}
+              >
                 <Select placeholder='Business Type' className='w-100'>
                     <Option value="Retailer">Retailer</Option>
                     <Option value="Freelancer">Freelancer</Option>
@@ -74,15 +71,31 @@ function Register() {
               </Form.Item>
             </Col>
           </Row>
-          <Form.Item name="bname" rules={[{ required: true, message: 'Please enter your Business Name ' },]}>
+          <Form.Item
+            name="bname"
+            rules={[
+              { required: true, message: 'Please enter your Business Name ' },
+            ]}
+          >
             <Input allowClear placeholder="Business Name"  />
           </Form.Item>
 
-          <Form.Item name="email" rules={[{ required: true, message: 'Please enter your Email id ' }, { type:'email' , message:"Enter a valid Email id" }]}>
+          <Form.Item
+            name="email"
+            rules={[
+              { required: true, message: 'Please enter your Email id ' },
+              { type:'email' , message:"Enter a valid Email id" }
+            ]}
+          >
             <Input allowClear placeholder="Email"  />
           </Form.Item>
 
-            <Form.Item name="mobile" rules={[{ required: true, message: 'Please enter your Mobile Number ' }]}>
+            <Form.Item
+                name="mobile"
+                rules={[
+                    { required: true, message: 'Please enter your Mobile Number ' }
+                ]}
+            >
                 <Input allowClear placeholder="Mobile"  />
             </Form.Item>
 
@@ -167,17 +180,17 @@ function Register() {
         <Typography className="text-center mt-5">
           <Text type="secondary"> OR </Text>
         </Typography>
-        <Row justify="space-between" className="mt-5 border-top">
-            <Col span={8} offset={1}>
-                <GoogleLoginButton style={{height:'6vh',borderRadius:'5vh',fontSize:'16px'}}   onClick={() => alert("Hello")}>
-                    <span>Google</span>
-                </GoogleLoginButton>
-            </Col>
-            <Col span={8}>
-                <FacebookLoginButton style={{height:'6vh',borderRadius:'5vh',fontSize:'16px'}}   onClick={() => alert("Hello")}>
-                    <span>Facebook</span>
-                </FacebookLoginButton>
-            </Col>
+        <Row justify="space-around" className="mt-5 border-top">
+          <Col span={6}>
+            <Button shape="round" icon={<GoogleOutlined />}>
+              Google
+            </Button>
+          </Col>
+          <Col span={6}>
+            <Button shape="round" icon={<FacebookOutlined />}>
+              Facebook
+            </Button>
+          </Col>
         </Row>
       </>
     )
