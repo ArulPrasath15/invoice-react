bd2e11e8633b4f0c3d65c2b9652f9eed7d97ea03
Yashwanth K <yashwanthkkn@gmail.com>
2021-08-07 12:08:59 +0530

    Added Header to client and new client route

--- pages/client/index.jsx
+++ pages/client/index.jsx
@@ -1,5 +1,6 @@
 import React, {useEffect, useState} from 'react'
 import Link from 'next/link'
+import Head from 'next/head'
 import ClientCard from "../../components/Client/ClientCard";
 import {Layout, Button, Typography, Col, Row, Table , Card, Space} from 'antd';
 import {PlusOutlined , UserOutlined, MailOutlined , PhoneOutlined} from '@ant-design/icons';
@@ -13,29 +14,35 @@ function Client() {
         setClients(data)
     },[])
     return (
-        <div className='mt-5 mx-5'>
-           <Row justify='space-between'  className='bg-white px-5 py-2 br-5'>
-                <Col span={8}>
-                    <Title level={4}  >Client</Title>
-                </Col>
-                <Col span={3} offset={12}>
-                    <Link href='/client/new'>
-                        <a>
-                            <Button type="primary"    icon={<PlusOutlined />} > Add Client</Button>
-                        </a>
-                    </Link>
-                </Col>
-           </Row>
-           <Row className="mt-5 mb-5" justify='space-between'>
-                {clients.map(client=>{
-                    return (
-                        <Col span={11} key={client.id} className="mt-5">
-                            <ClientCard client={client}  />
-                        </Col>
-                    )
-                })}
-           </Row>
-        </div>
+        <>
+            <Head>
+                <title>Client | Penta Invoice</title>
+                <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+            </Head>
+            <div className='mt-5 mx-5'>
+               <Row justify='space-between'  className='bg-white px-5 py-2 br-5'>
+                    <Col span={8}>
+                        <Title level={4}  >Client</Title>
+                    </Col>
+                    <Col span={3} offset={12}>
+                        <Link href='/client/new'>
+                            <a>
+                                <Button type="primary"    icon={<PlusOutlined />} > Add Client</Button>
+                            </a>
+                        </Link>
+                    </Col>
+               </Row>
+               <Row className="mt-5 mb-5" justify='space-between'>
+                    {clients.map(client=>{
+                        return (
+                            <Col span={11} key={client.id} className="mt-5">
+                                <ClientCard client={client}  />
+                            </Col>
+                        )
+                    })}
+               </Row>
+            </div>
+        </>
     )
 }
 
--- pages/client/new.jsx
+++ pages/client/new.jsx
@@ -2,28 +2,36 @@ import React from 'react';
 import ClientForm from "../../components/Client/ClientForm";
 import {Button, Col, Row, Typography} from "antd";
 import {PlusOutlined} from "@ant-design/icons";
-const { Title, Text } = Typography;
-
+import Head from "next/head";
 function NewClient(props) {
     return (
-        <div className='mt-5 mx-5'>
-            <Row justify='space-between'  className='bg-white px-5 py-2 br-5'>
-                <Col span={8}>
-                    <Title level={4}  >Add New Client</Title>
-                </Col>
-                <Col span={3} offset={12}>
-                    <Button type="danger" > Cancel</Button>
-                </Col>
-            </Row>
-            <div className="form">
-                <Row >
-                    <Col span={20} offset={2} className='bg-white mt-5 mb-5 py-5 px-5 br-5'>
-                        <ClientForm />
+        <>
+            <Head>
+                <title>New Client | Penta Invoice</title>
+                <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+            </Head>
+            <div className='mt-5 mx-5'>
+                <Row justify='space-between'  className='bg-white px-5 py-2 br-5'>
+                    <Col span={8}>
+                        <Title level={4}  >Add New Client</Title>
+                    </Col>
+                    <Col span={3} offset={12}>
+                        <Button type="danger" > Cancel</Button>
                     </Col>
                 </Row>
+                <div className="form">
+                    <Row >
+                        <Col span={20} offset={2} className='bg-white mt-5 mb-5 py-5 px-5 br-5'>
+                            <ClientForm />
+                        </Col>
+                    </Row>
+                </div>
             </div>
-        </div>
-    );
+        </>
+
+);
 }
+
+const { Title, Text } = Typography;
 
 export default NewClient;
