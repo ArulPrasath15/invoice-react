554d296ec763f0aa66b140014226cef2d0684ad5
Abinash S <s.abinash@outlook.com>
2021-08-07 12:12:36 +0530

    Added Timesheet Template

---
+++ components/Timesheet/New.jsx
@@ -0,0 +1,16 @@
+/*
+* @created: 07/08/2021 - 12:10 PM
+* @author: Abi
+* @description: ----------------
+*/
+import React from 'react';
+
+const NewTimeSheet = () => {
+    return (
+        <>
+            New Timesheet
+        </>
+    );
+};
+
+export default NewTimeSheet;
--- pages/timesheet/index.jsx
+++ pages/timesheet/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import notify from '../../components/Utils/notify';
+import { Empty } from 'antd';
 
 import {Layout, Button, Typography, Col, Row, Table , Card, Space} from 'antd';
 import {PlusOutlined , UserOutlined, MailOutlined , PhoneOutlined} from '@ant-design/icons';
@@ -7,6 +8,7 @@ const { Title, Text } = Typography;
 
 function Timesheet() {
     return (
+        <>
         <div className='mt-5 mx-5'>
            <Row justify='space-between'  className='bg-white px-5 py-2 br-5'>
                 <Col span={8}>
@@ -17,6 +19,16 @@ function Timesheet() {
                 </Col>
            </Row>
         </div>
+        <div className='mt-5 mx-5'>
+           <Row justify='center' align="middle"  className='bg-white px-5 py-2 br-5' style={{minHeight: '60vh'}}>
+               <Empty image="https://gw.alipayobjects.com/zos/antfincdn/ZHrcdLPrvN/empty.svg"
+                      imageStyle={{height: 80,}}
+                      description={<><Title level={4}>Track your work time</Title><Text type="secondary">Create timesheets for different clients and turn them into invoices.</Text></>}>
+                   <Button type="primary" icon={<PlusOutlined />}>Add Timesheet</Button>
+               </Empty>
+           </Row>
+        </div>
+        </>
     )
 }
 
