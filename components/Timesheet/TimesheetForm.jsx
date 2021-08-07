89c731b8213c3b409caeff88ab18dd18af6d8e5a
Abinash S <s.abinash@outlook.com>
2021-08-07 13:34:23 +0530

    Timesheet creation form

---
+++ components/Timesheet/TimesheetForm.jsx
@@ -0,0 +1,68 @@
+/*
+* @created: 07/08/2021 - 12:10 PM
+* @author: Abi
+* @description: ----------------
+*/
+import React, {useState} from 'react';
+import {Row, Col, Card, Form, Button, Select, Input} from "antd";
+import Link from 'next/link';
+import {PlusOutlined} from "@ant-design/icons";
+
+const { Option } = Select;
+
+const TimesheetForm = () => {
+
+    const [form] = Form.useForm();
+    const [requiredMark, setRequiredMarkType] = useState('');
+
+    return (
+        <>
+            <div className='mt-5 mx-5'>
+                <Row justify='center' align="middle" className='bg-white px-5 py-2 br-5' style={{minHeight: '60vh'}}>
+                    <Card title="Add Timesheet" bordered={false} style={{ width: '40vw' }}>
+                        <Form
+                            form={form}
+                            layout="vertical"
+                            requiredMark={requiredMark}
+                        >
+                            <Form.Item label="Client" required>
+                                <Select placeholder="Select a Client" allowClear>
+                                    <Option value="male">Client 1</Option>
+                                    <Option value="female">Client 2</Option>
+                                    <Option value="other"><Link href="#">Add Client</Link></Option>
+                                </Select>
+                            </Form.Item>
+                            <Form.Item label="Title" required>
+	                                <Input />
+                            </Form.Item>
+                            <Row gutter={8}>
+                                <Col span={12}>
+                                    <Form.Item label="Currency" required>
+                                        <Select showSearch placeholder="Select a person" optionFilterProp="children" filterOption={(input, option) => option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0}>
+                                            <Option value="jack">Jack</Option>
+                                            <Option value="lucy">Lucy</Option>
+                                            <Option value="tom">Tom</Option>
+                                        </Select>
+                                    </Form.Item>
+                                </Col>
+                                <Col span={12}>
+                                    <Form.Item label="Budget" requiredMark={'optional'}>
+                                        <Input placeholder="Title" />
+                                    </Form.Item>
+                                </Col>
+                            </Row>
+
+                            <Form.Item>
+                                <Button type="primary">Create</Button>
+                            </Form.Item>
+                        </Form>
+
+                    </Card>
+
+                </Row>
+            </div>
+        </>
+    );
+};
+
+export default TimesheetForm;
--- pages/timesheet/index.jsx
+++ pages/timesheet/index.jsx
@@ -1,33 +1,46 @@
-import React from 'react'
+import React, {useState} from 'react'
 import notify from '../../components/Utils/notify';
 import { Empty } from 'antd';
 
 import {Layout, Button, Typography, Col, Row, Table , Card, Space} from 'antd';
 import {PlusOutlined , UserOutlined, MailOutlined , PhoneOutlined} from '@ant-design/icons';
+import Head from "next/head";
+import New from "./new";
 const { Title, Text } = Typography;
 
 function Timesheet() {
+    const [isEmpty, setIsEmpty] = useState(false);
     return (
         <>
+        <Head>
+            <title>Timesheet | Penta Invoice</title>
+            <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        </Head>
         <div className='mt-5 mx-5'>
            <Row justify='space-between'  className='bg-white px-5 py-2 br-5'>
                 <Col span={8}>
-                    <Title level={4}  >Timesheet</Title>
+                    <Title level={4}>Timesheet</Title>
                 </Col>
                 <Col span={4} offset={11}>
                 <Button type="primary" icon={<PlusOutlined />} onClick={()=>notify({type:'success',msg:'Not yet Available'})}>Add Timesheet</Button>
                 </Col>
            </Row>
         </div>
-        <div className='mt-5 mx-5'>
-           <Row justify='center' align="middle"  className='bg-white px-5 py-2 br-5' style={{minHeight: '60vh'}}>
-               <Empty image="https://gw.alipayobjects.com/zos/antfincdn/ZHrcdLPrvN/empty.svg"
-                      imageStyle={{height: 80,}}
-                      description={<><Title level={4}>Track your work time</Title><Text type="secondary">Create timesheets for different clients and turn them into invoices.</Text></>}>
-                   <Button type="primary" icon={<PlusOutlined />}>Add Timesheet</Button>
-               </Empty>
-           </Row>
-        </div>
+        {
+            isEmpty && <div className='mt-5 mx-5'>
+            <Row justify='center' align="middle" className='bg-white px-5 py-2 br-5' style={{minHeight: '60vh'}}>
+                <Empty image="https://gw.alipayobjects.com/zos/antfincdn/ZHrcdLPrvN/empty.svg"
+                       imageStyle={{height: 80,}}
+                       description={<><Title level={4}>Track your work time</Title><Text type="secondary">Create
+                           timesheets for different clients and turn them into invoices.</Text></>}>
+                    <Button type="primary" icon={<PlusOutlined/>}>Add Timesheet</Button>
+                </Empty>
+            </Row>
+             </div>
+        }
+        {
+            !isEmpty && <New/>
+        }
         </>
     )
 }
