e0139e370dd827230c33420891b64085728b2e0c
Arul Prasath <arulprasath73@outlook.com>
2021-08-08 14:34:56 +0530

    Changes in General settings

--- components/Settings/General.jsx
+++ components/Settings/General.jsx
@@ -5,70 +5,98 @@ const { Title,Text } = Typography;
 const { TextArea } = Input;
 import Styles from '../../assets/css/General.module.css'
 import React, {useState} from "react";
-export function General() {
+const { Option } = Select;
+import notify from  '../../components/Utils/notify'
+
+
+
+
+function General({countryData}) {
+
+    const tempdata={
+        accountType: "Business",
+        businessName: "Vspace",
+        city: "Namakkal",
+        country: "India",
+        phoneNumber: "+919994198353",
+        state: "Tamil Nadu",
+        street: "4/450 ",
+        zip: "637001"
+    }
     const [editing, setEditing] = useState(false);
     const [acctype, setAcctype] = useState('Freelancer');
+    const [formdata, setFormdata] = useState(tempdata);
 
     const onEdit = () => {
         setEditing(!editing)
         console.log(editing)
     }
-    const AccTypeOptions = [
-        { label: 'Freelancer', value: 'Freelancer' },
-        { label: 'Business', value: 'Business' }
-    ];
+    const AccTypeOptions = [{ label: 'Freelancer', value: 'Freelancer' }, { label: 'Business', value: 'Business' }];
+
     const onChangeAccType = e => {
         console.log(' checked', e.target.value);
         setAcctype(e.target.value);
     };
     const onSubmit = (values)=>{
-        console.log(values)
+        setFormdata(values)
+        notify({type:'success',msg:'Updated successfully',des:'General settings updated successfully'})
+        setEditing(!editing)
     }
+
+
     return (
         <>
             {!editing &&
             <div>
-                    <Row justify="end"><Button type="primary" icon={<EditOutlined/>} onClick={() => {
-                        onEdit()
-                    }}>Edit</Button></Row>
-                    <Divider className="mt-3 mb-2"/>
+                <Row justify="end"><Button type="primary" icon={<EditOutlined/>} onClick={() => {
+                    onEdit()
+                }}>Edit</Button></Row>
+                <Divider className="mt-3 mb-2"/>
+                <div style={{paddingLeft:"20vw",paddingRight:"20vw"}}>
+                        <Row><Title level={5}>ACCOUNT </Title></Row>
+                        <hr className={Styles.hr}/>
+                        <Row justify="start">
+                            <Text>Account Type : </Text>
+                            <Text className='px-2' strong>{formdata.accountType}</Text>
+                        </Row>
+                        {formdata.businessName  &&
+                        <Row justify="start">
+                            <Text>Business Name : </Text>
+                            <Text className='px-2' strong>{formdata.businessName}</Text>
+                        </Row>
+                        }
 
-                    <Row><Title level={5}>ACCOUNT </Title></Row>
-                    <hr className={Styles.hr}/>
-                    <Row justify="start">
-                        <Text>Account Type : </Text>
-                        <Text className='px-2' strong>Freelancer</Text>
-                    </Row>
+                        <Row className='pt-5'><Title level={5}>ADDRESS </Title></Row>
+                        <hr className={Styles.hr}/>
+                        <Row justify="start" className='pt-1'>
+                            <Text>Street/Number : </Text>
+                            <Text className='px-2' strong>{formdata.street}</Text>
+                        </Row>
+                        <Row justify="start" className='pt-1'>
+                            <Text>ZIP : </Text>
+                            <Text className='px-2' strong>{formdata.zip}</Text>
+                        </Row>
+                        <Row justify="start" className='pt-1'>
+                            <Text>City : </Text>
+                            <Text className='px-2' strong>{formdata.city}</Text>
+                        </Row>
+                        <Row justify="start" className='pt-1'>
+                            <Text>State : </Text>
+                            <Text className='px-2' strong>{formdata.state}</Text>
+                        </Row>
+                        <Row justify="start" className='pt-1'>
+                            <Text>Country : </Text>
+                            <Text className='px-2' strong>{formdata.country}</Text>
+                        </Row>
 
-                    <Row className='pt-5'><Title level={5}>ADDRESS </Title></Row>
-                    <hr className={Styles.hr}/>
-                    <Row justify="start" className='pt-1'>
-                        <Text>Street/Number : </Text>
-                        <Text className='px-2' strong>5/450</Text>
-                    </Row>
-                    <Row justify="start" className='pt-1'>
-                        <Text>ZIP : </Text>
-                        <Text className='px-2' strong>637001</Text>
-                    </Row>
-                    <Row justify="start" className='pt-1'>
-                        <Text>City : </Text>
-                        <Text className='px-2' strong>Namakkal</Text>
-                    </Row>
-                    <Row justify="start" className='pt-1'>
-                        <Text>State : </Text>
-                        <Text className='px-2' strong>Tamil Nadu</Text>
-                    </Row>
-                    <Row justify="start" className='pt-1'>
-                        <Text>Country : </Text>
-                        <Text className='px-2' strong>India</Text>
-                    </Row>
+                        <Row className='pt-5'><Title level={5}>CONTACT INFORMATION </Title></Row>
+                        <hr className={Styles.hr}/>
+                        <Row justify="start">
+                            <Text>Phone number : </Text>
+                            <Text className='px-2' strong>{formdata.phoneNumber}</Text>
+                        </Row>
+                    </div>
 
-                    <Row className='pt-5'><Title level={5}>CONTACT INFORMATION </Title></Row>
-                    <hr className={Styles.hr}/>
-                    <Row justify="start">
-                        <Text>Phone number : </Text>
-                        <Text className='px-2' strong>9994646466</Text>
-                    </Row>
             </div>
             }
 
@@ -88,16 +116,17 @@ export function General() {
                 </Row>
                 <Divider className="mt-3 mb-2"/>
 
+                    <div style={{paddingLeft:"20vw",paddingRight:"20vw"}}>
                 <Row><Title level={5}>ACCOUNT </Title></Row>
                 <hr className={Styles.hr}/>
                 <Row justify="start" className='pt-2'>
-                    <Text className='pt-1'>Account Type </Text>
-                    <Form.Item  name="acctType" rules={[{ required: true, message: 'Please enter Business Type ' }]}>
-                        <Radio.Group className='px-5' options={AccTypeOptions} value={acctype} onChange={onChangeAccType} optionType="button"/>
+                    <Text className='pt-1'><span className='px-1' style={{color:"red"}}>*</span>Account Type </Text>
+                    <Form.Item  name="accountType" rules={[{ required: true, message: 'Please enter Business Type ' }]}>
+                        <Radio.Group className='px-5' options={AccTypeOptions} value={acctype}  onChange={onChangeAccType} optionType="button" buttonStyle="solid"/>
                     </Form.Item>
                     {acctype==='Business' &&
                     <Col>
-                        <Form.Item  name="businessname" rules={[{ required: true, message: 'Please enter Business Name ' }]}>
+                        <Form.Item  name="businessName" rules={[{ required: true, message: 'Please enter Business Name ' }]}>
                             <Input placeholder="Business Name" prefix={<ShopOutlined />}/>
                         </Form.Item>
 
@@ -129,8 +158,10 @@ export function General() {
                     </Col>
                     <Col span={11}>
                         <Form.Item name="country" label="Country" rules={[{ required: true }]}>
-                            <Select placeholder="Country" allowClear>
-                                <Option value="India">India</Option>
+                            <Select placeholder="Country" allowClear showSearch filterOption={(input, option) => option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0}>
+                                {countryData.map(country => (
+                                    <Option value={country.countryName} key={country.countryName}>{country.countryName}</Option>
+                                ))}
                             </Select>
                         </Form.Item>
                     </Col>
@@ -139,15 +170,18 @@ export function General() {
                 <Row><Title level={5}>CONTACT INFORMATION </Title></Row>
                 <hr className={Styles.hr}/>
                 <Row justify="start" className='pt-2'>
-                    <Col span={11}>
-                        <Form.Item label="Phone Number" name="phonenumber" rules={[{ required: true, message: 'Please enter Phone Number ' }]}>
-                            <Input />
+                    <Col span={12}>
+                        <Form.Item label="Phone Number" name="phoneNumber" rules={[{ required: true, message: 'Please enter Phone Number ' }]}>
+                            <Input  />
                         </Form.Item>
                     </Col>
                 </Row>
-                </Form>
+                </div>
+               </Form>
             </div>
             }
         </>
     )
 }
+
+export default General;
--- pages/settings/index.jsx
+++ pages/settings/index.jsx
@@ -5,11 +5,30 @@
 */
 import React from 'react';
 import {Tabs} from "antd";
-import {General} from "../../components/Settings/General";
+import General from "../../components/Settings/General";
 import {Accounting} from "../../components/Settings/Accounting";
 import Head from "next/head";
 const { TabPane } = Tabs;
-const Settings = () => {
+
+export async function getServerSideProps(context) {
+    const res = await fetch('https://restcountries.eu/rest/v2/all');
+    let countries = await res.json();
+    let data=[];
+    await countries.forEach(country=>{
+        if(country.name && country.callingCodes[0] )
+            data.push({
+                countryName: country.name,
+                countryCallingCodes: country.callingCodes[0],
+                countryCode: country.alpha3Code,
+            });
+    });
+    return {
+        props: {data}, // will be passed to the page component as props
+    }
+}
+
+
+const Settings = ({data}) => {
     return (
         <>
         <Head>
@@ -19,7 +38,7 @@ const Settings = () => {
             <div className="card-container">
                 <Tabs type="card" size='large' >
                     <TabPane tab="General" key="1">
-                        <General/>
+                        <General countryData={data}/>
                     </TabPane>
                     <TabPane tab="Accounting"  key="2">
                         <Accounting/>
