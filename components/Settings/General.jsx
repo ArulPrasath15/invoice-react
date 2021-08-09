37bec99f9d5eb3d58b2ecc7187f7a3b7a47a314d
Yashwanth K <yashwanthkkn@gmail.com>
2021-08-09 11:03:47 +0530

    Updated the General settings UI

--- components/Settings/General.jsx
+++ components/Settings/General.jsx
@@ -1,4 +1,4 @@
-import {Row, Col, Button, Divider, Radio, Input, Form, Select} from 'antd';
+import {Row, Col, Button, Divider, Radio, Input, Form, Select, Space} from 'antd';
 import { EditOutlined,SaveOutlined,CloseOutlined,ShopOutlined} from '@ant-design/icons';
 import { Typography } from 'antd';
 const { Title,Text } = Typography;
@@ -48,54 +48,58 @@ function General({countryData}) {
         <>
             {!editing &&
             <div>
-                <Row justify="end"><Button type="primary" icon={<EditOutlined/>} onClick={() => {
-                    onEdit()
-                }}>Edit</Button></Row>
+                <Row justify="end">
+                    <Button type="primary" icon={<EditOutlined/>}
+                            onClick={() => {onEdit()}}>Edit
+                    </Button>
+                </Row>
                 <Divider className="mt-3 mb-2"/>
-                <div style={{paddingLeft:"20vw",paddingRight:"20vw"}}>
-                        <Row><Title level={5}>ACCOUNT </Title></Row>
-                        <hr className={Styles.hr}/>
-                        <Row justify="start">
-                            <Text>Account Type : </Text>
+                <div style={{paddingRight:"20vw",paddingLeft:"20vw"}}>
+                    <Title level={5} type={'secondary'} >ACCOUNT </Title>
+                    <Space direction={'vertical'}>
+                        <Space>
+                            <Text className={'text-secondary'}>Account Type : </Text>
                             <Text className='px-2' strong>{formdata.accountType}</Text>
-                        </Row>
+                        </Space>
                         {formdata.businessName  &&
-                        <Row justify="start">
-                            <Text>Business Name : </Text>
+                        <Space >
+                            <Text className={'text-secondary'}>Business Name : </Text>
                             <Text className='px-2' strong>{formdata.businessName}</Text>
-                        </Row>
+                        </Space>
                         }
+                    </Space>
 
-                        <Row className='pt-5'><Title level={5}>ADDRESS </Title></Row>
-                        <hr className={Styles.hr}/>
-                        <Row justify="start" className='pt-1'>
-                            <Text>Street/Number : </Text>
+                    <Title className={'mt-5'} level={5} type={'secondary'}>ADDRESS </Title>
+                    <Space direction={'vertical'}>
+                        <Space>
+                            <Text className={'text-secondary'}>Street/Number : </Text>
                             <Text className='px-2' strong>{formdata.street}</Text>
-                        </Row>
-                        <Row justify="start" className='pt-1'>
-                            <Text>ZIP : </Text>
-                            <Text className='px-2' strong>{formdata.zip}</Text>
-                        </Row>
-                        <Row justify="start" className='pt-1'>
-                            <Text>City : </Text>
-                            <Text className='px-2' strong>{formdata.city}</Text>
-                        </Row>
-                        <Row justify="start" className='pt-1'>
-                            <Text>State : </Text>
-                            <Text className='px-2' strong>{formdata.state}</Text>
-                        </Row>
-                        <Row justify="start" className='pt-1'>
-                            <Text>Country : </Text>
-                            <Text className='px-2' strong>{formdata.country}</Text>
-                        </Row>
+                        </Space>
 
-                        <Row className='pt-5'><Title level={5}>CONTACT INFORMATION </Title></Row>
-                        <hr className={Styles.hr}/>
-                        <Row justify="start">
-                            <Text>Phone number : </Text>
-                            <Text className='px-2' strong>{formdata.phoneNumber}</Text>
-                        </Row>
-                    </div>
+                        <Space >
+                            <Text className={'text-secondary'}>ZIP : </Text>
+                            <Text className='px-2' strong>{formdata.zip}</Text>
+                        </Space>
+                        <Space>
+                            <Text className={'text-secondary'}>City : </Text>
+                            <Text className='px-2' strong>{formdata.city}</Text>
+                        </Space>
+                        <Space>
+                            <Text className={'text-secondary'}>State : </Text>
+                            <Text className='px-2' strong>{formdata.state}</Text>
+                        </Space>
+                        <Space>
+                            <Text className={'text-secondary'}>Country : </Text>
+                            <Text className='px-2' strong>{formdata.country}</Text>
+                        </Space>
+                    </Space>
+
+                    <Title level={5} type={'secondary'} className={'mt-5'}>CONTACT INFORMATION </Title>
+                    <Space justify="start">
+                        <Text className={'text-secondary'}>Phone number : </Text>
+                        <Text className='px-2' strong>{formdata.phoneNumber}</Text>
+                    </Space>
+                </div>
 
             </div>
             }
@@ -104,79 +108,79 @@ function General({countryData}) {
             <div>
                 <Form layout="vertical"
                       name="basic"
-                      onFinish={onSubmit}>
+                      onFinish={onSubmit}
+                >
                 <Row justify="space-between">
                     <Col>
-                        <Text>Edit your general account settings</Text>
+                        <Text className={'text-secondary'}>Edit your general account settings</Text>
                     </Col>
-                   <Col>
+                    <Col>
                        <Button type="primary" icon={<SaveOutlined />} htmlType="submit" >Save</Button>
                        <Button className='mx-2' type="dashed" icon={<CloseOutlined />} onClick={() => {onEdit()}}>Cancel</Button>
-                   </Col>
+                    </Col>
                 </Row>
                 <Divider className="mt-3 mb-2"/>
 
-                    <div style={{paddingLeft:"20vw",paddingRight:"20vw"}}>
-                <Row><Title level={5}>ACCOUNT </Title></Row>
-                <hr className={Styles.hr}/>
-                <Row justify="start" className='pt-2'>
-                    <Text className='pt-1'><span className='px-1' style={{color:"red"}}>*</span>Account Type </Text>
-                    <Form.Item  name="accountType" rules={[{ required: true, message: 'Please enter Business Type ' }]}>
-                        <Radio.Group className='px-5' options={AccTypeOptions} value={acctype}  onChange={onChangeAccType} optionType="button" buttonStyle="solid"/>
+                <div style={{paddingLeft:"20vw",paddingRight:"20vw"}}>
+
+                    <Title level={5} type={'secondary'}>ACCOUNT </Title>
+                    <Row justify="start" className='pt-2'>
+                        <Text className='pt-1'><span className='px-1' style={{color:"red"}}>*</span>Account Type </Text>
+                        <Form.Item  name="accountType" rules={[{ required: true, message: 'Please enter Business Type ' }]}>
+                            <Radio.Group className='px-5' options={AccTypeOptions} value={acctype}  onChange={onChangeAccType} optionType="button" buttonStyle="solid"/>
+                        </Form.Item>
+
+                        {acctype==='Business' &&
+                        <Col>
+                            <Form.Item  name="businessName" rules={[{ required: true, message: 'Please enter Business Name ' }]}>
+                                <Input placeholder="Business Name" prefix={<ShopOutlined />}/>
+                            </Form.Item>
+                        </Col>}
+                    </Row>
+
+                    <Title level={5} type={'secondary'}>ADDRESS </Title>
+                    <Form.Item className='pt-3' label="Street / Number" name="street" rules={[{ required: true, message: 'Please enter Street Name ' }]}>
+                        <Input />
                     </Form.Item>
-                    {acctype==='Business' &&
-                    <Col>
-                        <Form.Item  name="businessName" rules={[{ required: true, message: 'Please enter Business Name ' }]}>
-                            <Input placeholder="Business Name" prefix={<ShopOutlined />}/>
-                        </Form.Item>
+                    <Row justify={"space-between"}>
+                        <Col span={11}>
+                            <Form.Item label="ZIP" name="zip" rules={[{ required: true, message: 'Please enter Zip code ' }]}>
+                                <Input />
+                            </Form.Item>
+                        </Col>
+                        <Col span={11}>
+                            <Form.Item label="City" name="city" rules={[{ required: true, message: 'Please enter City Name ' }]}>
+                                <Input />
+                            </Form.Item>
+                        </Col>
+                    </Row>
+                    <Row justify={"space-between"}>
+                        <Col span={11}>
+                            <Form.Item label="State" name="state" rules={[{ required: true, message: 'Please enter State Name ' }]}>
+                                <Input />
+                            </Form.Item>
+                        </Col>
+                        <Col span={11}>
+                            <Form.Item name="country" label="Country" rules={[{ required: true }]}>
+                                <Select placeholder="Country" allowClear showSearch filterOption={(input, option) => option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0}>
+                                    {countryData.map(country => (
+                                        <Option value={country.countryName} key={country.countryName}>{country.countryName}</Option>
+                                    ))}
+                                </Select>
+                            </Form.Item>
+                        </Col>
+                    </Row>
 
-                    </Col>}
-                </Row>
-
-                <Row className='pt-4'> <Title level={5}>ADDRESS </Title></Row>
-                <hr className={Styles.hr}  />
-                <Form.Item className='pt-3' label="Street / Number" name="street" rules={[{ required: true, message: 'Please enter Street Name ' }]}>
-                    <Input />
-                </Form.Item>
-                <Row justify={"space-between"}>
-                    <Col span={11}>
-                        <Form.Item label="ZIP" name="zip" rules={[{ required: true, message: 'Please enter Zip code ' }]}>
-                            <Input />
-                        </Form.Item>
-                    </Col>
-                    <Col span={11}>
-                        <Form.Item label="City" name="city" rules={[{ required: true, message: 'Please enter City Name ' }]}>
-                            <Input />
-                        </Form.Item>
-                    </Col>
-                </Row>
-                <Row justify={"space-between"}>
-                    <Col span={11}>
-                        <Form.Item label="State" name="state" rules={[{ required: true, message: 'Please enter State Name ' }]}>
-                            <Input />
-                        </Form.Item>
-                    </Col>
-                    <Col span={11}>
-                        <Form.Item name="country" label="Country" rules={[{ required: true }]}>
-                            <Select placeholder="Country" allowClear showSearch filterOption={(input, option) => option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0}>
-                                {countryData.map(country => (
-                                    <Option value={country.countryName} key={country.countryName}>{country.countryName}</Option>
-                                ))}
-                            </Select>
-                        </Form.Item>
-                    </Col>
-                </Row>
-
-                <Row><Title level={5}>CONTACT INFORMATION </Title></Row>
-                <hr className={Styles.hr}/>
-                <Row justify="start" className='pt-2'>
-                    <Col span={12}>
-                        <Form.Item label="Phone Number" name="phoneNumber" rules={[{ required: true, message: 'Please enter Phone Number ' }]}>
-                            <Input  />
-                        </Form.Item>
-                    </Col>
-                </Row>
+                    <Title level={5}  type={'secondary'}>CONTACT INFORMATION </Title>
+                    <Row justify="start" className='pt-2'>
+                        <Col span={12}>
+                            <Form.Item label="Phone Number" name="phoneNumber" rules={[{ required: true, message: 'Please enter Phone Number ' }]}>
+                                <Input  />
+                            </Form.Item>
+                        </Col>
+                    </Row>
                 </div>
+
                </Form>
             </div>
             }
