1302b968e7e3437f37777923db7205d900385a3a
Yashwanth K <yashwanthkkn@gmail.com>
2021-08-10 18:54:30 +0530

    Minor UI updates in Auth Ui

--- pages/auth.jsx
+++ pages/auth.jsx
@@ -19,47 +19,18 @@ function Auth() {
             <title>Pentafox Invoice</title>
             <meta name="vieport" content="initial-scale=1.0, width=device-width" />
         </Head>
-            {/*<Row align="middle" className={styles.authBgImg}>*/}
-            {/*    <Col span={16} offset={4}>*/}
-            {/*        <Card bordered={false} className={styles.authCard}>*/}
-            {/*            <Row>*/}
-            {/*                <Col span={0} lg={12}>*/}
-            {/*                    <Space>*/}
-            {/*                        <Image src={logoImg} alt="img" width={35} height={35} className={styles.authLogo} />*/}
-            {/*                        <h1>Pentafox</h1>*/}
-            {/*                    </Space>*/}
-            {/*                    <Image src={VectorImg} alt="img" className={styles.authVectorImg} />*/}
-            {/*                </Col>*/}
-            {/*                <Col sm={24} lg={12}>*/}
-            {/*                    <Row justify="space-around">*/}
-            {/*                        <Col span={6}>*/}
-            {/*                            <Button danger={showLogin} type="text"  onClick={()=>setLogin(true)}>Login</Button>*/}
-            {/*                        </Col>*/}
-            {/*                        <Col span={6}>*/}
-            {/*                            <Button  danger={!showLogin} type="text" onClick={()=>setLogin(false)}>Signup</Button>*/}
-            {/*                        </Col>*/}
-            {/*                    </Row>*/}
-            {/*                    { showLogin && <Login />}*/}
-            {/*                    { !showLogin && <Register />}*/}
-            {/*                </Col>*/}
-            {/*            </Row>*/}
-            {/*        </Card>*/}
-            {/*    </Col>*/}
-            {/*</Row>*/}
 
-
-
-            <Row  align={"middle"}>
+            <Row>
                 <Col sm={0} lg={12} className={styles.authBgImg}>
                 </Col>
                 <Col sm={24} lg={12} style={{paddingLeft:"7vw",paddingRight:"7vw"}}>
-                    <div>
-                        <Space style={{paddingBottom:"5vh"}}>
+                    <div className={'mt-3'}>
+                        <Space>
                             <Image src={logoImg} alt="img" width={35} height={35} className={styles.authLogo} />
                             <h1>Pentafox Invoice</h1>
                         </Space>
                     </div>
-                    <Row justify="space-around" className='py-5' >
+                    <Row justify="space-around" className='mt-5' >
                         <Col span={6}>
                             <Button danger={showLogin} type="text"  onClick={()=>setLogin(true)}>Login</Button>
                         </Col>
