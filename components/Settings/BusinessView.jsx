63e3f0c88e804b68c884a31afc99a60f8f6ffcb7
Arul Prasath <arulprasath73@outlook.com>
2021-08-24 13:49:28 +0530

    Connected business API

--- components/Settings/BusinessView.jsx
+++ components/Settings/BusinessView.jsx
@@ -4,90 +4,89 @@ import { Typography } from 'antd';
 const { Title,Text } = Typography;
 const { TextArea } = Input;
 import Styles from '../../assets/css/General.module.css'
-import React, {useState} from "react";
+import React, {useState,useEffect} from "react";
 const { Option } = Select;
 import notify from  '../../components/Utils/notify'
+import {getSession} from "next-auth/client";
+import axios from "axios";
 
 
-function BusinessView({countryData,onEdit ,handleSelection}) {
+function BusinessView({countryData,onEdit ,handleSelection,generalData}) {
 
-    const formdata = {
-        accountType: "Business",
-        businessName: "Vspace",
-        city: "Namakkal",
-        country: "India",
-        phoneNumber: "+919994198353",
-        state: "Tamil Nadu",
-        street: "4/450 ",
-        zip: "637001"
-    }
+
+    // console.log(generalData)
+
     return (
         <>
-            <div>
+            {
+                generalData &&
+                <div>
                 <Row justify="end">
                     <Col>
                         <Button type="primary" icon={<PlusOutlined/>}
-                                onClick={() => {handleSelection('new')}}>New
+                                onClick={() => {
+                                    handleSelection('new')
+                                }}>New
                         </Button>
                     </Col>
-                    <Col style={{marginLeft:'10px'}}>
+                    <Col style={{marginLeft: '10px'}}>
                         <Button type="primary" icon={<EditOutlined/>}
-                                onClick={() => {handleSelection('edit')}}>Edit
+                                onClick={() => {
+                                    handleSelection('edit')
+                                }}>Edit
                         </Button>
                     </Col>
                 </Row>
                 <Divider className="mt-3 mb-2"/>
                 <div>
-                    <Row justify={'around'}>
+                    <Row justify={'space-around'}>
                         <Col span={12}>
-                            <Title className={'mt-5'}  level={5} type={'secondary'} >ACCOUNT </Title>
+                            <Title className={'mt-5'} level={5} type={'secondary'}>ACCOUNT </Title>
                             <Space direction={'vertical'} className={'mt-3'}>
                                 <Space>
                                     <Text className={'text-secondary'}>Account Type : </Text>
-                                    <Text className='px-2' strong>{formdata.accountType}</Text>
+                                    <Text className='px-2' strong>{generalData.business.business_type}</Text>
                                 </Space>
-                                {formdata.businessName  &&
-                                <Space >
+                                <Space>
                                     <Text className={'text-secondary'}>Business Name : </Text>
-                                    <Text className='px-2' strong>{formdata.businessName}</Text>
+                                    <Text className='px-2' strong>{generalData.business.business_name}</Text>
                                 </Space>
-                                }
                             </Space>
-                            <Title level={5} type={'secondary'} style={{marginTop:'40px'}}>CONTACT INFORMATION </Title>
+                            <Title level={5} type={'secondary'} style={{marginTop: '40px'}}>CONTACT INFORMATION </Title>
                             <Space justify="start" className={'mt-3'}>
                                 <Text className={'text-secondary'}>Phone number : </Text>
-                                <Text className='px-2' strong>{formdata.phoneNumber}</Text>
+                                <Text className='px-2' strong>{generalData.business.phone_number}</Text>
                             </Space>
                         </Col>
                         <Col>
                             <Title className={'mt-5'} level={5} type={'secondary'}>ADDRESS </Title>
                             <Space direction={'vertical'} className={'mt-3'}>
-                                <Space>
+                                <Row>
                                     <Text className={'text-secondary'}>Street/Number : </Text>
-                                    <Text className='px-2' strong>{formdata.street}</Text>
-                                </Space>
+                                    <Text className='px-2' strong>{generalData.business.address}</Text>
+                                </Row>
 
-                                <Space >
+                                <Space>
                                     <Text className={'text-secondary'}>ZIP : </Text>
-                                    <Text className='px-2' strong>{formdata.zip}</Text>
+                                    <Text className='px-2' strong>{generalData.business.pincode}</Text>
                                 </Space>
                                 <Space>
                                     <Text className={'text-secondary'}>City : </Text>
-                                    <Text className='px-2' strong>{formdata.city}</Text>
+                                    <Text className='px-2' strong>{generalData.business.city}</Text>
                                 </Space>
                                 <Space>
                                     <Text className={'text-secondary'}>State : </Text>
-                                    <Text className='px-2' strong>{formdata.state}</Text>
+                                    <Text className='px-2' strong>{generalData.business.state}</Text>
                                 </Space>
                                 <Space>
                                     <Text className={'text-secondary'}>Country : </Text>
-                                    <Text className='px-2' strong>{formdata.country}</Text>
+                                    <Text className='px-2' strong>{generalData.business.country}</Text>
                                 </Space>
                             </Space>
                         </Col>
                     </Row>
                 </div>
-            </div>
+            </div>}
         </>
     )
 }
--- components/Settings/General.jsx
+++ components/Settings/General.jsx
@@ -4,30 +4,24 @@ import { Typography } from 'antd';
 const { Title,Text } = Typography;
 const { TextArea } = Input;
 import Styles from '../../assets/css/General.module.css'
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 const { Option } = Select;
 import notify from  '../../components/Utils/notify'
 import BusinessView from './BusinessView'
 import BusinessForm from './BusinessForm'
+import {getSession} from "next-auth/client";
+import axios from "axios";
 
 
 
 function General({countryData}) {
 
-    const tempdata={
-        accountType: "Business",
-        businessName: "Vspace",
-        city: "Namakkal",
-        country: "India",
-        phoneNumber: "+919994198353",
-        state: "Tamil Nadu",
-        street: "4/450 ",
-        zip: "637001"
-    }
+    const [userId, setUserId] = useState();
     const [editing, setEditing] = useState(false);
     const [showNew, setNew] = useState(false);
     const [acctype, setAcctype] = useState('Freelancer');
-    const [formdata, setFormdata] = useState(tempdata);
+    const [generalData, setGeneralData] = useState();
+    const currentBusinessId="611b74c8a9a37b2a6cca07cf"
 
     const onEdit = () => {
         setEditing(!editing)
@@ -54,17 +48,49 @@ function General({countryData}) {
         console.log(' checked', e.target.value);
         setAcctype(e.target.value);
     };
-    const onSubmit = (values)=>{
-        setFormdata(values)
-        notify({type:'success',msg:'Updated successfully',des:'General settings updated successfully'})
-        setEditing(!editing)
+    const onSubmit = async(values)=> {
+
+        const params={'bid':currentBusinessId, 'updates':values}
+        try{
+            const res = await axios.put('/business', params);
+            if (res.status===200 && res.data.user_data ) {
+                const businessData={business:res.data.user_data}
+                setGeneralData(businessData)
+                notify({type:'success',msg:'Updated successfully',des:'General settings updated successfully'})
+                setEditing(!editing)
+            }
+            else {
+                notify({type:'warning',msg:'Failed ',des:'General settings updation Failed'})
+                setEditing(!editing)
+            }
+        }catch(err){
+            notify({type:'warning',msg:'Failed ',des:'General settings updation Failed'})
+            setEditing(!editing)
+        }
     }
+
+    useEffect(()=>{
+        getSession().then(async (session,loading) => {
+            if (session ) {
+                setUserId(session.user.user.id)
+                console.log('General User ID '+session.user.user.id)
+                try{
+                    const res = await axios.get('/auth/getUser/'+session.user.user.id );
+                    if (res.status===200) {
+                        const res = await axios.get('/business/'+currentBusinessId );
+                        setGeneralData(res.data)
+                    }
+                }catch(err){
+                }
+            }
+        });
+    },[])
 
 
     return (
         <>
 
-            {!editing && !showNew && <BusinessView onEdit={onEdit} handleSelection={handleSelection} />}
+            {!editing && generalData && !showNew && <BusinessView generalData = {generalData} onEdit={onEdit} handleSelection={handleSelection} />}
             {showNew &&
             <div>
                 <Row justify="space-between">
@@ -77,13 +103,12 @@ function General({countryData}) {
                 </Row>
                 <Divider className="mt-3 mb-2"/>
                 <BusinessForm countryData={countryData}/>
-                </div>}
+                </div>
+            }
+
             {editing &&
             <div>
-                <Form layout="vertical"
-                      name="basic"
-                      onFinish={onSubmit}
-                >
+                <Form layout="vertical" name="basic" onFinish={onSubmit}>
                     <Row justify="space-between">
                         <Col>
                             <Title level={4} type={'secondary'}>Edit your general account settings</Title>
@@ -99,42 +124,37 @@ function General({countryData}) {
 
                     <Title level={5} type={'secondary'}>ACCOUNT </Title>
                     <Row justify="start" className='pt-2'>
-                        <Form.Item  name="accountType" label={"Account Type"} rules={[{ required: true, message: 'Please enter Business Type ' }]}>
+                        <Form.Item  name="business_type" initialValue={generalData.business.business_type} label={"Account Type"} rules={[{ required: true, message: 'Please enter Business Type ' }]}>
                             <Radio.Group className='px-0' options={AccTypeOptions} value={acctype}  onChange={onChangeAccType} optionType="button" buttonStyle="solid"/>
                         </Form.Item>
                     </Row>
-
-
-                            <Form.Item  name="businessName" rules={[{ required: true, message: 'Please enter Business Name ' }]}>
-                                <Input placeholder="Business Name" prefix={<ShopOutlined />}/>
-                            </Form.Item>
-
-
-
+                    <Form.Item  name="business_name" initialValue={generalData.business.business_name} rules={[{ required: true, message: 'Please enter Business Name ' }]}>
+                        <Input placeholder="Business Name" prefix={<ShopOutlined />}/>
+                    </Form.Item>
                     <Title level={5} type={'secondary'}>ADDRESS </Title>
-                    <Form.Item className='pt-3' label="Street / Number" name="street" rules={[{ required: true, message: 'Please enter Street Name ' }]}>
+                    <Form.Item className='pt-3' initialValue={generalData.business.address}  label="Street / Number" name="address" rules={[{ required: true, message: 'Please enter Street Name ' }]}>
                         <Input />
                     </Form.Item>
                     <Row justify={"space-between"}>
                         <Col span={11}>
-                            <Form.Item label="ZIP" name="zip" rules={[{ required: true, message: 'Please enter Zip code ' }]}>
+                            <Form.Item label="ZIP" initialValue={generalData.business.pincode} name="pincode" rules={[{ required: true, message: 'Please enter Zip code ' }]}>
                                 <Input />
                             </Form.Item>
                         </Col>
                         <Col span={11}>
-                            <Form.Item label="City" name="city" rules={[{ required: true, message: 'Please enter City Name ' }]}>
+                            <Form.Item label="City" initialValue={generalData.business.city}  name="city" rules={[{ required: true, message: 'Please enter City Name ' }]}>
                                 <Input />
                             </Form.Item>
                         </Col>
                     </Row>
                     <Row justify={"space-between"}>
                         <Col span={11}>
-                            <Form.Item label="State" name="state" rules={[{ required: true, message: 'Please enter State Name ' }]}>
+                            <Form.Item label="State" initialValue={generalData.business.state}  name="state" rules={[{ required: true, message: 'Please enter State Name ' }]}>
                                 <Input />
                             </Form.Item>
                         </Col>
                         <Col span={11}>
-                            <Form.Item name="country" label="Country" rules={[{ required: true }]}>
+                            <Form.Item name="country" initialValue={generalData.business.country} label="Country" rules={[{ required: true }]}>
                                 <Select placeholder="Country" allowClear showSearch filterOption={(input, option) => option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0}>
                                     {countryData.map(country => (
                                         <Option value={country.countryName} key={country.countryName}>{country.countryName}</Option>
@@ -147,7 +167,7 @@ function General({countryData}) {
                     <Title level={5}  type={'secondary'}>CONTACT INFORMATION </Title>
                     <Row justify="start" className='pt-2'>
                         <Col span={12}>
-                            <Form.Item label="Phone Number" name="phoneNumber" rules={[{ required: true, message: 'Please enter Phone Number ' }]}>
+                            <Form.Item label="Phone Number" initialValue={generalData.business.phone_number} name="phone_number" rules={[{ required: true, message: 'Please enter Phone Number ' }]}>
                                 <Input  />
                             </Form.Item>
                         </Col>
