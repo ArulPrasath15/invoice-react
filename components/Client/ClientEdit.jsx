244f1b0573b2351b29c045e8b6a7ca6990d3b82c
Yashwanth K <yashwanthkkn@gmail.com>
2021-09-02 14:36:11 +0530

    client profile can be updated now

---
+++ components/Client/ClientEdit.jsx
@@ -0,0 +1,224 @@
+/*
+* @created: 02/09/2021 - 1:55 PM
+* @author: Yash
+* @description: Form component to edit client
+*/
+import React from 'react';
+import {Row, Col, Typography, Space, Button, Form, Input, Select, message , Divider} from 'antd';
+import {SaveOutlined,CloseOutlined} from '@ant-design/icons';
+import axios from 'axios'
+import {useRouter } from 'next/router'
+const { Title, Text } = Typography;
+const { Option } = Select;
+
+
+function ClientEdit(props) {
+    const Router  = useRouter()
+    const {countryData,client,setClient,setEdit} = props
+
+
+    const onSubmit = async (values)=>{
+        const params = {id:client._id,updates:values};
+        const hide=message.loading('Please wait...',0);
+        try{
+            const res = await axios.put('/client',params);
+            if(res.status === 200){
+                hide();
+                message.success('Successfully Updated Client',2);
+                setClient(res.data.client);
+                setEdit(false);
+            }else{
+                throw Error(res.data.msg)
+            }
+        }catch (err){
+            hide();
+            if (!err.response) {
+                console.log("Custom Network Error",err)
+                message.error('Network Error',2);
+            } else {
+                message.error(err.message,2)
+            }
+        }
+    }
+    return (
+
+        <Form
+            layout="vertical"
+            name="basic"
+            onFinish={onSubmit}
+        >
+
+            <Row justify={'space-between'}>
+                <Col>
+                    <Title type='secondary' level={5}>CLIENT DETAILS</Title>
+                </Col>
+                <Col>
+                    <Form.Item >
+                        <Button type="primary" htmlType="submit" icon={<SaveOutlined />}>
+                            Save
+                        </Button>
+                        <Button type={'text'} icon={<CloseOutlined />} danger onClick={()=>{setEdit(false)}}>Discard</Button>
+                    </Form.Item>
+                </Col>
+            </Row>
+            <Form.Item
+                label="Client Business Name"
+                name="business_name"
+                rules={[{ required: true, message: 'Please enter Client Name ' }]}
+                initialValue={client.business_name}
+            >
+                <Input />
+            </Form.Item>
+
+            <Form.Item
+                label="GSTIN"
+                name="gstin"
+                rules={[{ required: true, message: 'Please enter GSTIN ' }]}
+                initialValue={client.gstin}
+            >
+                <Input />
+            </Form.Item>
+
+            <div>
+                <Title type='secondary' level={5}>CONTACT PERSON</Title>
+                <Divider />
+            </div>
+            <Row justify={"space-between"}>
+                <Col span={11}>
+                    <Form.Item
+                        label="First Name"
+                        name="first_name"
+                        rules={[{ required: true, message: 'Please enter First Name ' }]}
+                        initialValue={client.first_name}
+                    >
+                        <Input />
+                    </Form.Item>
+                </Col>
+                <Col span={11}>
+                    <Form.Item
+                        label="Last Name"
+                        name="last_name"
+                        rules={[{ required: true, message: 'Please enter Last Name ' }]}
+                        initialValue={client.last_name}
+                    >
+                        <Input />
+                    </Form.Item>
+                </Col>
+            </Row>
+            <Form.Item
+                name='email'
+                label="Email"
+                rules={[
+                    { type: 'email' , message:"Please enter valid email"},
+                    { required:true , message:"Please enter Email"}
+                ]}
+                initialValue={client.email}
+            >
+                <Input />
+            </Form.Item>
+            <small className={'text-secondary'}>Invoices will be sent to this Email</small>
+            <Form.Item
+                name="phone"
+                label="Phone Number"
+                initialValue={client.phone}
+            >
+                <Input style={{ width: '100%' }} />
+            </Form.Item>
+
+            <div className="mt-3">
+                <Title type='secondary' level={5}>BILLING ADDRESS</Title>
+                <Divider />
+            </div>
+
+            <Form.Item
+                label="Address"
+                name="address"
+                rules={[{ required: true, message: 'Please enter Street Name  ' }]}
+                initialValue={client.address}
+            >
+                <Input />
+            </Form.Item>
+            <Row justify={"space-between"}>
+                <Col span={11}>
+                    <Form.Item
+                        label="Pincode"
+                        name="pincode"
+                        rules={[{ required: true, message: 'Please enter Pincode ' }]}
+                        initialValue={client.pincode}
+                    >
+                        <Input />
+                    </Form.Item>
+                </Col>
+                <Col span={11}>
+                    <Form.Item
+                        label="City"
+                        name="city"
+                        rules={[{ required: true, message: 'Please enter City Name ' }]}
+                        initialValue={client.city}
+                    >
+                        <Input />
+                    </Form.Item>
+                </Col>
+            </Row>
+            <Row justify={"space-between"}>
+                <Col span={11}>
+                    <Form.Item
+                        label="State"
+                        name="state"
+                        rules={[{ required: true, message: 'Please enter State Name ' }]}
+                        initialValue={client.state}
+                    >
+                        <Input />
+                    </Form.Item>
+                </Col>
+                <Col span={11}>
+                    <Form.Item name="country" label="Country" rules={[{ required: true }]} initialValue={client.country} >
+                        <Select
+                            placeholder="Country"
+                            allowClear
+                            showSearch
+                            filterOption={(input, option) => option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0}
+                        >
+                            {countryData.length > 0 &&
+                            countryData.map(country => {
+                                return (
+                                    <Option value={country.countryName} key={country.countryCode}>{country.countryName}</Option>
+                                )
+                            })
+                            }
+                        </Select>
+                    </Form.Item>
+                </Col>
+            </Row>
+            <div className="mt-3">
+                <Title type='secondary' level={5}>ADDITIONAL INFO</Title>
+                <Divider />
+            </div>
+            <Row justify='space-between'>
+                <Col span='11'>
+                    <Form.Item
+                        name='website'
+                        label="Website"
+                        initialValue={client.website}
+                    >
+                        <Input />
+                    </Form.Item>
+                </Col>
+                <Col span='11'>
+                    <Form.Item
+                        name="vatid"
+                        label="VAT ID"
+                        initialValue={client.vatid}
+                    >
+                        <Input />
+                    </Form.Item>
+                </Col>
+            </Row>
+
+        </Form>
+    );
+}
+
+
+
+export default ClientEdit;
